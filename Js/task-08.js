374c466c5a746fa4116defee1495b38907c062d1
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-01-13 09:33:18 +0200

    Have fixed 8 task

--- Js/task-08.js
+++ Js/task-08.js
@@ -1,12 +1,11 @@
 import users from "/users.js";
 
 const getUsersWithFriend = (users, friendName) => {
-  users = users.reduce((acc, member) => {
-    member.friends.includes(friendName) && acc.push(member.name);
-    return acc;
-  }, []);
-  return users;
+  users = users.filter(user => user.friends.includes(friendName));
+const arrayOfFriends= users.map(member => member.name);
+return arrayOfFriends;
 };
 
-console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
+
